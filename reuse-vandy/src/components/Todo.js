d5e7424b2e9845b31c19d7c82eab688263fc4684
avivroskes <avivroskes@gmail.com>
2024-01-31 11:26:51 -0600

    working on my local system

--- reuse-vandy/src/components/Todo.js
+++ reuse-vandy/src/components/Todo.js
@@ -1,42 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import { db } from '../services/firebase.config';
-import { collection, addDoc, serverTimestamp, getDocs, doc, deleteDoc, runTransaction, orderBy, query } from 'firebase/firestore';
-import EditTodo from './EditTodo';
+import React, { useState, useEffect } from 'react'
+import { collection, addDoc, serverTimestamp, getDocs, doc, deleteDoc, runTransaction, orderBy, query } from 'firebase/firestore'
+import EditTodo from './EditTodo'
+
+import { db } from '../services/firebase.config'
 
 const Todo = () => {
-  const [createTodo, setCreateTodo] = useState("");
-  const [checked, setChecked] = useState([]);
-  const collectionRef = collection(db, 'todo');
+
+  const [createTodo, setCreateTodo] = useState("")
   const [todos, setTodo] = useState([]);
 
-  // Delete Handler
-  const deleteTodo = async (id) => {
+  const [checked, setChecked] = useState([]);
+
+  const collectionRef = collection(db, 'todo')
+
+  useEffect(() => {
+    const getTodo = async () => {
+      const q = query(collectionRef, orderBy('timestamp'))
+      await getDocs(q).then((todo) => {
+        let todoData = todo.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        setTodo(todoData)
+        setChecked(todoData)
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+    getTodo()
+  }, [])
+
+
+  //Add Todo Handler
+  const submitTodo = async (e) => {
+    e.preventDefault();
+
     try {
-      const confirmDelete = window.confirm("Are you sure you want to delete this Todo?");
-      if (confirmDelete) {
-        const documentRef = doc(db, "todo", id);
-        await deleteDoc(documentRef);
-        window.location.reload();
-      }
+      await addDoc(collectionRef, {
+        todo: createTodo,
+        isChecked: false,
+        timestamp: serverTimestamp()
+      })
+      window.location.reload();
     } catch (err) {
       console.log(err);
     }
-  };
+  }
 
- // Checkbox Handler
-const checkHandler = async (event, todo) => {
+  console.log(todos);
+
+  //Delete Handler
+  const deleteTodo = async (id) => {
     try {
-      const docRef = doc(db, "todo", event.target.name);
+
+      if (window.confirm("Are you sure you want to delete this Task!")) {
+        const documentRef = doc(db, "todo", id);
+        await deleteDoc(documentRef)
+        window.location.reload()
+      }
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+
+  const checkHandler = async (event, todo) => {
+    try {
+      const docRef = doc(db, 'todo', event.target.name);
       const transactionResult = await runTransaction(db, async (transaction) => {
         const todoDoc = await transaction.get(docRef);
         if (!todoDoc.exists()) {
-          throw "Document does not exist!";
+          throw 'Document does not exist!';
         }
         const newValue = !todoDoc.data().isChecked;
         transaction.update(docRef, { isChecked: newValue });
         return newValue;
       });
-  
+
       setChecked((state) => {
         const indexToUpdate = state.findIndex(
           (checkBox) => checkBox.id.toString() === event.target.name
@@ -49,42 +88,10 @@ const checkHandler = async (event, todo) => {
         setTodo(newState);
         return newState;
       });
-  
-      console.log("Transaction successfully committed!");
+
+      console.log('Transaction successfully committed!');
     } catch (error) {
-      console.log("Transaction failed: ", error);
-    }
-  };
-  
-
-  useEffect(() => {
-    const getTodo = async () => {
-      try {
-        // Create a query to order todos by timestamp
-        const q = query(collectionRef, orderBy('timestamp'));
-        const todoSnapshot = await getDocs(q);
-        const todoData = todoSnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-        setTodo(todoData);
-        setChecked(todoData);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getTodo();
-  }, [collectionRef]);
-
-  // Add Todo Handler
-  const submitTodo = async (e) => {
-    e.preventDefault();
-    try {
-      await addDoc(collectionRef, {
-        todo: createTodo,
-        isChecked: false,
-        timestamp: serverTimestamp(),
-      });
-      window.location.reload();
-    } catch (err) {
-      console.log(err);
+      console.log('Transaction failed: ', error);
     }
   };
 
@@ -99,44 +106,39 @@ const checkHandler = async (event, todo) => {
                   data-bs-toggle="modal"
                   data-bs-target="#addModal"
                   type="button"
-                  className="btn btn-info"
-                >
-                  Add Todo
+                  className="btn btn-info">Add Todo
                 </button>
 
-                <div className="todo-list">
-                  {todos.map(({ todo, id, isChecked, timestamp }) => (
-                    <div className="todo-list" key={id}>
-                      <div className="todo-item">
-                        <hr />
-                        <span className={`${isChecked === true ? 'done' : ''}`}>
-                          <div className="checker">
-                            <span className="">
-                              <input
-                                type="checkbox"
-                                defaultChecked={isChecked}
-                                name={id}
-                                onChange={(event) => checkHandler(event, todo)}
-                              />
-                            </span>
-                          </div>
-                          &nbsp;{todo}<br />
-                          <i>{new Date(timestamp.seconds * 1000).toLocaleString()}</i>
-                        </span>
-                        <span className="float-end mx-3">
-                          <EditTodo todo={todo} id={id} />
-                        </span>
-                        <button
-                          type="button"
-                          className="btn btn-danger float-end"
-                          onClick={() => deleteTodo(id)}
-                        >
-                          Delete
-                        </button>
-                      </div>
+                {todos.map(({ todo, id, isChecked, timestamp }) =>
+                  <div className="todo-list" key={id}>
+                    <div className="todo-item">
+                      <hr />
+                      <span className={`${isChecked === true ? 'done' : ''}`}>
+                        <div className="checker" >
+                          <span className="" >
+                            <input
+                              type="checkbox"
+                              defaultChecked={isChecked}
+                              name={id}
+                              onChange={(event) => checkHandler(event, todo)}
+                            />
+                          </span>
+                        </div>
+                        &nbsp;{todo}<br />
+                        <i>{new Date(timestamp.seconds * 1000).toLocaleString()}</i>
+                      </span>
+                      <span className=" float-end mx-3">
+                        <EditTodo todo={todo} id={id} />
+                      </span>
+                      <button
+                        type="button"
+                        className="btn btn-danger float-end"
+                        onClick={() => deleteTodo(id)}
+                      >Delete</button>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                )}
+
               </div>
             </div>
           </div>
@@ -144,26 +146,13 @@ const checkHandler = async (event, todo) => {
       </div>
 
       {/* Modal */}
-      <div
-        className="modal fade"
-        id="addModal"
-        tabIndex="-1"
-        aria-labelledby="addModalLabel"
-        aria-hidden="true"
-      >
+      <div className="modal fade" id="addModal" tabIndex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
         <div className="modal-dialog">
           <form className="d-flex" onSubmit={submitTodo}>
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="addModalLabel">
-                  Add Todo
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
+                <h5 className="modal-title" id="addModalLabel">Add Todo</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
                 <input
@@ -174,12 +163,8 @@ const checkHandler = async (event, todo) => {
                 />
               </div>
               <div className="modal-footer">
-                <button
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
+                <button className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
                 <button className="btn btn-primary">Create Todo</button>
               </div>
             </div>
@@ -187,7 +172,7 @@ const checkHandler = async (event, todo) => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Todo;
+export default Todo
--- reuse-vandy/src/services/firebase.config.js
+++ reuse-vandy/src/services/firebase.config.js
@@ -1,8 +1,7 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+
 
 export const db = getFirestore(app);
