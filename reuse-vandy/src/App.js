48d43230769e01cb359c42bd00b174b28f513d40
lynchgl <gwen.l.lynch@vanderbilt.edu>
2024-03-20 13:29:53 -0500

    get rid of old category pages

--- reuse-vandy/src/App.js
+++ reuse-vandy/src/App.js
@@ -6,10 +6,6 @@ import SellItem from './components/SellItem/SellItem';
 import SignIn from './components/SignIn/SignIn';
 import SignUp from './components/SignUp/SignUp';
 import Profile from './components/Profile/Profile'
-import FurniturePage from './components/FurniturePage/FurniturePage';
-import ClothingPage from './components/ClothingPage/ClothingPage';
-import TechnologyPage from './components/TechnologyPage/TechnologyPage';
-import TextbooksPage from './components/TextbooksPage/TextbooksPage';
 import TrendingPage from './components/TrendingPage/TrendingPage';
 import CategoryPage from './components/CategoryPage/CategoryPage';
 
@@ -25,10 +21,6 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/furniture" element={<FurniturePage />} />
-        <Route path="/clothing" element={<ClothingPage />} />
-        <Route path="/technology" element={<TechnologyPage />} />
-        <Route path="/textbooks" element={<TextbooksPage />} />
         <Route path="/trending" element={<TrendingPage />} />
         <Route path="/category" element={<CategoryPage />} />
       </Routes>
