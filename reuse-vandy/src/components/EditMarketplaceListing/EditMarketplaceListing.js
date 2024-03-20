2b21021c51c01752d0e225ada9ad610b7bc02a82
Madeline King <madeline.n.king@vanderbilt.edu>
2024-03-20 14:30:14 -0500

    working

--- reuse-vandy/src/components/EditMarketplaceListing/EditMarketplaceListing.js
+++ reuse-vandy/src/components/EditMarketplaceListing/EditMarketplaceListing.js
@@ -1,21 +1,40 @@
 import React, { useState } from 'react';
 import { doc, updateDoc } from 'firebase/firestore';
 import { dbMarketplaceListings, auth } from '../../services/firebase.config';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import './EditMarketplaceListing.css'
 
 const EditMarketplaceListing = ({ listing, categories }) => {
   const [editedListing, setEditedListing] = useState({ ...listing });
+  const [image, setImage] = useState(null)
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
 
   const handleEdit = async (e) => {
     e.preventDefault();
 
     try {
       const documentRef = doc(dbMarketplaceListings, 'listings', listing.id);
-      await updateDoc(documentRef, {
+      const updates = {
         title: editedListing.title,
         price: editedListing.price,
         category: editedListing.category,
-      });
+      };
+
+      // If there's a new image, upload it and update the image URL
+      if (image) {
+        const storage = getStorage();
+        const storageRef = ref(storage);
+        const imageRef = ref(storageRef, `listing_images/${listing.id}-${Date.now()}`);
+        await uploadBytes(imageRef, image);
+        const imageUrl = await getDownloadURL(imageRef);
+        updates.imageUrl = imageUrl;
+      }
+
+      await updateDoc(documentRef, updates);
 
       // Reload the page after saving changes
       window.location.reload();
@@ -89,7 +108,7 @@ const EditMarketplaceListing = ({ listing, categories }) => {
                     type="file"
                     className="form-control"
                     id={`imageInput-${listing.id}`}
-                    onChange={(e) => setEditedListing({ ...editedListing, image: parseFloat(e.target.value) })}
+                    onChange={(e) => handleImageUpload(e)}
                   />
                 </div>
               </div>
